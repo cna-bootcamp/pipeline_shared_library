7ba9a82c6fba132dd18be1f5d95766d47a605d3d
ondal <hiondal@gmail.com>
2024-07-29 15:02:40 +0900

    update

--- src/com/subride/PipelineLibraries.groovy
+++ src/com/subride/PipelineLibraries.groovy
@@ -125,7 +125,7 @@ class PipelineLibraries implements Serializable {
 
     //-- 소스 변경 여부 검사: 멀티 프로젝트이므로 타 서비스 소스 변경 시에도 파이프라인이 실행되어 검사 필요
     def checkSourceChanges() {
-        if(!envVars.SKIP_STAGES.contains("src")) return true    //source변경여부 체크 안함
+        if(envVars.SKIP_STAGES.contains("src")) return true    //source변경여부 체크 안함
 
         if (envVars.SERVICE_GROUP == envVars.SERVICE_GROUP_SUBRIDE_FRONT) return true   //프로트엔드는 검사 불필요
 
